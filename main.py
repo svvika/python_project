21ca9d5cdbd90c3e9f7f6447e19bcf48be2340aa
klochkovaso <superklochkova2005@gmail.com>
2024-05-04 23:39:16 +0300

    window

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Hello")
-#kekekekkek
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((1000, 700))
+pygame.display.set_caption("tajna")
+running = True
+while running:
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
