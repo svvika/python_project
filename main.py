60962a697ea44a097d047452e11928488bcc9671
svvika <slivavika@yandex.ru>
2024-05-16 17:04:16 +0300

    main.py pep-8

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import engine
 import village
 import insects
-engine.init({"village":village.village,"insects":insects.insects})
+
+engine.init({"village": village.village, "insects": insects.insects})
 engine.run("village")
