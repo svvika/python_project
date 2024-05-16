277a298510f1225b87167e740bcee37faa4a29c1
svvika <slivavika@yandex.ru>
2024-05-16 16:08:10 +0300

    mini game ogorod new picture and grid size

--- ogorod_info.py
+++ ogorod_info.py
@@ -6,7 +6,7 @@ WIDTH = 600
 HEIGHT = 600
 SCREEN_SIZE = (WIDTH, HEIGHT)
 
-TILE_SIZE = 60
+TILE_SIZE = 75
 TILE_NUM_W = WIDTH // TILE_SIZE
 TILE_NUM_H = HEIGHT // TILE_SIZE
 
@@ -17,7 +17,7 @@ FPS = 15
 # Optimal for tile size 60 (10 tiles/row) = (FPS - 2) * 2
 # ? Optimal for tile size 75 (8 tiles/row) = (FPS - 4) * 2
 if TILE_SIZE == 75:
-    TPS = (FPS - 4) * 2
+    TPS = (FPS - 5) * 2
 elif TILE_SIZE == 60:
     TPS = (FPS - 2) * 2
 
