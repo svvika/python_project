0138b8391a6a1854dacf0d70831227b74f95519c
svvika <slivavika@yandex.ru>
2024-05-18 01:10:27 +0300

    ogorod=carrots mini game engine version

--- ogorod_info.py
+++ ogorod_info.py
@@ -2,6 +2,8 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BROWN = (75, 37, 0)
 
+TEXT_SIZE = 24
+
 WIDTH = 600
 HEIGHT = 600
 SCREEN_SIZE = (WIDTH, HEIGHT)
@@ -14,11 +16,7 @@ PLAYER = "character2.png"
 OBJECT = "carrot.png"
 
 FPS = 15
-# Optimal for tile size 60 (10 tiles/row) = (FPS - 2) * 2
-# ? Optimal for tile size 75 (8 tiles/row) = (FPS - 4) * 2
 if TILE_SIZE == 75:
     TPS = (FPS - 4) * 2
-elif TILE_SIZE == 60:
-    TPS = (FPS - 2) * 2
 
 WIN_COUNT = 30
