e4a75ecdb0e1083981c5a4dbac2255975fe15033
svvika <slivavika@yandex.ru>
2024-05-18 04:52:11 +0300

    updated prologue and readme

--- prologue.py
+++ prologue.py
@@ -4,42 +4,54 @@ import random
 import math
 import json
 
+
 def prologue(state):
     if not state["initialised"]:
         state["prologue"] = dict()
-        char_list = json.loads(open("characters.json","r",encoding="utf-8").read())
-        char_no = random.randint(0,3)
+        char_list = json.loads(open("characters.json", "r", encoding="utf-8").read())
+        char_no = random.randint(0, 3)
         state["progress"] = {
             "character": list(char_list.keys())[char_no],
             "character_location": list(char_list.values())[char_no],
-            "insects1" : {"available": False,"attempts": 3,"completed" : False},
-            "insects2" : {"available": False,"attempts": 3,"completed" : False},
-            "stars1" : {"available": False, "completed" : False},
-            "stars2" : {"available": False, "completed" : False},
-            "carrots1" : {"available": False, "completed" : False},
-            "carrots2" : {"available": False, "completed" : False},
-            "mushrooms" : {"available": False, "completed" : False},
-            "products" : {"available": False, "completed" : False},
-            "water" : {"available": False, "completed" : False},
-            "house1" : {"visited": False, "completed" : False},
-            "house2" : {"visited": False},
-            "house3" : {"visited": False},
-            "house4" : {"visited": False},
-            "house5" : {"visited": False},
-            "house6" : {"visited": False, "completed" : False},
-            "house7" : {"visited": False, "completed" : False},
-            "house8" : {"visited": False},
-            "house9" : {"visited": False},
-            "notes" : [False,False,False,False,False,
-                       False,False,False,False]
+            "insects1": {"available": False, "attempts": 3, "completed": False},
+            "insects2": {"available": False, "attempts": 3, "completed": False},
+            "stars1": {"available": False, "completed": False},
+            "stars2": {"available": False, "completed": False},
+            "carrots1": {"available": False, "completed": False},
+            "carrots2": {"available": False, "completed": False},
+            "mushrooms": {"available": False, "completed": False},
+            "products": {"available": False, "completed": False},
+            "water": {"available": False, "completed": False},
+            "house1": {"visited": False, "completed": False},
+            "house2": {"visited": False},
+            "house3": {"visited": False},
+            "house4": {"visited": False},
+            "house5": {"visited": False},
+            "house6": {"visited": False, "completed": False},
+            "house7": {"visited": False, "completed": False},
+            "house8": {"visited": False},
+            "house9": {"visited": False},
+            "notes": [False, False, False, False, False, False, False, False, False]
         }
         ps = state["prologue"]
-        ps["scene"] = engine.Scene(size=(800, 600),  sprite_named_filenames={"default":"images/prologue_bg.jpg"})
-        greeting_text = "Добро пожаловать! Вам нужно найти {0}".format(state["progress"]["character"])
-        hint_text = "Движение — стрелками, действие — стрелкой вверх.\nНажмите ВВЕРХ, чтобы начать."
+        ps["scene"] = engine.Scene(size=(800, 600), sprite_named_filenames={"default": "prologue_bg.jpg"})
+        hello_text = "Добро пожаловать!"
+        greeting_text = ("Вы студент-участник экспедиции в деревню, в округе которой, как ходят слухи, обитают лесные "
+                         "существа из древнерусской мифологии. Ваша цель - доказать, что существует {0}. Найти этого "
+                         "персонажа вам помогут местные жители, готовые рассказать, что знают, за небольшую помощь. "
+                         "Искать следует в месте, которое чаще всего упоминается как место этого"
+                         "существа.").format(state["progress"]["character"])
+        goodluck_text = "Удачи!"
+        hint_text = "ДВИЖЕНИЕ - СТРЕЛКИ ВПРАВО И ВЛЕВО; ДЕЙСТВИЕ - СТРЕЛКА ВВЕРХ."
+        switch_text = "НАЗАД ИЗ ЛЕСА - СТРЕЛКА ВНИЗ. НАЖМИТЕ ВВЕРХ, ЧТОБЫ НАЧАТЬ."
+        text_x = 25
+        text_w = 740
         ps["texts"] = {
-            "greeting": engine.Text((0,0),size=36,width=800,text=greeting_text,font_name="advent.ttf"),
-            "hint": engine.Text((0,500),size=24,width=800,text=hint_text,font_name="advent.ttf")
+            "hello": engine.Text((text_x, 85), size=29, width=text_w, text=hello_text, font_name="advent.ttf"),
+            "greeting": engine.Text((text_x, 125), size=24, width=text_w, text=greeting_text, font_name="advent.ttf"),
+            "goodluck": engine.Text((text_x, 245), size=24, width=text_w, text=goodluck_text, font_name="advent.ttf"),
+            "hint": engine.Text((7, 562), size=18, width=800, text=hint_text, font_name="advent.ttf"),
+            "switch": engine.Text((7, 582), size=18, width=800, text=switch_text, font_name="advent.ttf")
         }
         state["initialised"] = True
     ps = state["prologue"]
@@ -48,6 +60,7 @@ def prologue(state):
         state["SWITCH"] = "village"
         del state["prologue"]
     ps["scene"].render(engine.SCREEN)
+
     for text in ps["texts"].values():
         text.render(engine.SCREEN)
     return state
