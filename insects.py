63835d14b82a8dd0a530effb3d198aac2142263c
Diana434 <144337064+Diana434@users.noreply.github.com>
2024-05-18 10:18:21 +0300

    новые насекомые

--- insects.py
+++ insects.py
@@ -3,6 +3,7 @@ import engine
 import random
 import math
 
+
 def fly_fate(state, fly_name, cloud_name=False):
     if not cloud_name:
         state["insects"]["lives"] -= 1
@@ -19,6 +20,7 @@ def fly_fate(state, fly_name, cloud_name=False):
 
     return state
 
+
 def insects(state):
     if not state["initialised"]:
         state["insects"] = dict()
@@ -29,31 +31,31 @@ def insects(state):
         state["insects"]["score"] = 40
         state["insects"]["entities"] = dict()
         state["insects"]["lives"] = 5
-        state["insects"]["scene"] = engine.Scene(size=(800, 600),  sprite_named_filenames={"default":"images/insects_bg.jpg",
-                                                                                          "end":"images/insects_bg_end.jpg"})
-        state["insects"]["entities"]["player"] = engine.GameEntity((375, 450), (51, 146), {"default":"images/gun2.png"})
-        state["insects"]["counter"] = engine.Text((10, 450), size=40, colour = "RED")
+        state["insects"]["scene"] = engine.Scene(size=(800, 600),  sprite_named_filenames={"default": "images/insects_bg.jpg",
+                                                                                           "end": "images/insects_bg_end.jpg"})
+        state["insects"]["entities"]["player"] = engine.GameEntity((375, 450), (51, 146), {"default": "images/gun2.png"})
+        state["insects"]["counter"] = engine.Text((10, 450), size=40, colour="RED")
         state["insects"]["switchdelay"] = 360
-        state["insects"]["end_text"] = engine.Text((40, 240), size=40, colour = "RED")
-
+        state["insects"]["end_text"] = engine.Text((40, 240), size=40, colour="RED")
+        state["village"]["music"] = pygame.mixer.music.load("sounds/insects.mp3")
+        pygame.mixer.music.play(-1)
         state["initialised"] = True
-    #print(state["progress"][state["current_task"]]["attempts"])
     if state["insects"]["lives"] <= 0 or state["insects"]["score"] <= 0:
         print("GO")
         state["insects"]["scene"].current_sprite = "end"
         if state["insects"]["lives"] <= 0:
-            state["insects"]["end_text"].text = "Мухи одолели Вас. Осталось попыток: "\
+            state["insects"]["end_text"].text = "Мухи одолели Вас.\nОсталось попыток: "\
             + str(state["progress"][state["current_task"]]["attempts"] - 1)
 
 
         else:
             state["insects"]["end_text"].text = "Вы победили!\nПриходите к жителю за запиской."
-            state["progress"][state["current_task"]] = {"available":False, "attempts": 0, "completed":True}
+            state["progress"][state["current_task"]] = {"available": False, "attempts": 9999, "completed": True}
         if state["insects"]["switchdelay"] <= 0:
             state["progress"][state["current_task"]]["attempts"] -= 1
             if state["progress"][state["current_task"]]["attempts"] <= 0:
-                state["progress"][state["current_task"]] = {"available":False, "completed":False}
-            del state["insects"] # нам больше не нужно текущее состояние этой миниигры
+                state["progress"][state["current_task"]] = {"available": False, "completed": False}
+            del state["insects"]  # нам больше не нужно текущее состояние этой миниигры
             state["SWITCH"] = "village"
         else:
             state["insects"]["switchdelay"] -= 1
@@ -65,8 +67,8 @@ def insects(state):
         if state["insects"]["flycooldown"] == 0:
             state["insects"]["flycount"] += 1
             state["insects"]["flycooldown"] = 30
-            fly = engine.Trigger((random.randint(0, 5)*150, -60), (64, 64),\
-            {"default":"images/bug.png"}, func=fly_fate, type={"collides"})
+            fly = engine.Trigger((random.randint(0, 5)*150, -60), (64, 64),
+                                 {"default": "images/bug.png"}, func=fly_fate, type={"collides"})
             fly.data["speedx"] = (350 - fly.pos[0])/350
             fly.data["speedy"] = (500 - fly.pos[1])/350
             fly.rot = (math.atan(fly.data["speedx"]/fly.data["speedy"])*180)/3.14
@@ -86,12 +88,10 @@ def insects(state):
         elif entity_name[:3] == "clo":
             cloud = state["insects"]["entities"][entity_name]
             cloud.pos = (cloud.pos[0]+cloud.data["speedx"], cloud.pos[1]+cloud.data["speedy"])
-
-
     for entity_name in state["insects"]["deathrow"]:
         try:
             del state["insects"]["entities"][entity_name]
-        except:
+        except Exception:
             print("I tried")
     state["insects"]["deathrow"] = []
 
@@ -101,10 +101,10 @@ def insects(state):
         state["insects"]["entities"]["player"].rot -= 2
 
     if state["insects"]["guncooldown"] < 0:
-        if state["k"][pygame.K_UP]: # должно исправить проблему с удержанием
+        if state["k"][pygame.K_UP]:  # должно исправить проблему с удержанием
             state["insects"]["guncooldown"] = 30
             ppos = state["insects"]["entities"]["player"].pos
-            cloud = engine.GameEntity((ppos[0]+12, ppos[1]), (64, 64), {"default":"images/cloud.png"})
+            cloud = engine.GameEntity((ppos[0]+12, ppos[1]), (64, 64), {"default": "images/cloud.png"})
             cloud.data["speedx"] = -math.sin(math.radians(state["insects"]["entities"]["player"].rot))*10
             cloud.data["speedy"] = -math.cos(math.radians(state["insects"]["entities"]["player"].rot))*10
             state["insects"]["entities"]["cloud"+str(cloud.id)] = cloud
